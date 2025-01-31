ff02ff81338cd652b380672663aaf1f76c4a4207
JordanTrindade <jordandimst@gmail.com>
2025-01-30 22:35:27 -0300

    Update TarefaService.java

--- src/main/java/com/app/todolist/model/task/service/TarefaService.java
+++ src/main/java/com/app/todolist/model/task/service/TarefaService.java
@@ -4,11 +4,10 @@ import com.app.todolist.model.task.Tarefa;
 import com.app.todolist.model.task.repository.TarefaRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @Service
 public class TarefaService {
@@ -16,22 +15,27 @@ public class TarefaService {
     @Autowired
     private TarefaRepository tarefaRepository;
 
-    public Tarefa salvarTarefa(Tarefa tarefa){
-        return tarefaRepository.save(tarefa);
+    public List<Tarefa> add(Tarefa tarefa){
+        tarefaRepository.save(tarefa);
+        return listarTodasTarefas();
     }
 
-    public void deletarTarefa(Long id){
+    public List<Tarefa> delete(Long id){
         Tarefa tarefaEncontrada = buscaTarefaPorId(id);
         tarefaRepository.delete(tarefaEncontrada);
-    }
-
-    public List<Tarefa> buscarTodasTarefas(){
-        return tarefaRepository.findAll();
+        return listarTodasTarefas();
     }
 
     public Tarefa buscaTarefaPorId(Long id){
         return tarefaRepository.findById(id).
                 orElseThrow(
                         ()-> new EntityNotFoundException(("Tarefa nao encontrada")));
+    }
+
+    public List<Tarefa> listarTodasTarefas(){
+        Sort sort = Sort.by("prioridade").descending().and(
+                Sort.by("titulo").ascending());
+
+        return tarefaRepository.findAll(sort);
     }
 }
