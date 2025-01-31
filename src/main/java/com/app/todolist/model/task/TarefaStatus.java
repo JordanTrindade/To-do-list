d0db29be73c48c0c4efe20e3bd91c6f229b2435a
JordanTrindade <jordandimst@gmail.com>
2025-01-30 22:35:41 -0300

    Update TarefaStatus.java

--- src/main/java/com/app/todolist/model/task/TarefaStatus.java
+++ src/main/java/com/app/todolist/model/task/TarefaStatus.java
@@ -3,13 +3,15 @@ package com.app.todolist.model.task;
 import jakarta.persistence.Entity;
 
 public enum TarefaStatus {
-    CONCLUIDO("concluido"),
-    PENDENTE("andamento"),
-    CANCELADA("cancelada");
+    CONCLUIDO(1,"concluido"),
+    PENDENTE( 2,"andamento"),
+    CANCELADA(3,"cancelada");
 
+    private Integer id;
     private String descricao;
 
-    TarefaStatus(String descricao){
+    TarefaStatus(Integer id, String descricao){
+        this.id = id;
         this.descricao = descricao;
     }
 }
