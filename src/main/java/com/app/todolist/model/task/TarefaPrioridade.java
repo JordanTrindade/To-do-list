20ac5cae599da3be428b72f2a2923f39cd5263ce
JordanTrindade <jordandimst@gmail.com>
2025-01-30 22:35:38 -0300

    Update TarefaPrioridade.java

--- src/main/java/com/app/todolist/model/task/TarefaPrioridade.java
+++ src/main/java/com/app/todolist/model/task/TarefaPrioridade.java
@@ -1,15 +1,17 @@
 package com.app.todolist.model.task;
 
 public enum TarefaPrioridade {
-
-        ALTA("Urgente"),
-        MEDIA("Verifcar"),
-        BAIXA("Fazer"),
-        INDEFINIDA("Quando possivel");
+        ALTA( "Urgente",1),
+        MEDIA("Verifcar",2),
+        BAIXA("Fazer",3),
+        INDEFINIDA("Quando possivel",4);
 
         private String descricao;
+        private Integer id;
 
-    TarefaPrioridade(String descricao){
+    TarefaPrioridade(String descricao, int id){
             this.descricao = descricao;
+            this.id = id;
     }
+
 }
