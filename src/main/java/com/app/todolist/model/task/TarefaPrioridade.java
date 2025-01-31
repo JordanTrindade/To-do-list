9eec0ef7001853983f51823549525f2a2883b18c
JordanTrindade <jordandimst@gmail.com>
2025-01-31 18:20:07 -0300

    Update TarefaPrioridade.java
    adicionando integerToTarefaPrioridade

--- src/main/java/com/app/todolist/model/task/TarefaPrioridade.java
+++ src/main/java/com/app/todolist/model/task/TarefaPrioridade.java
@@ -14,4 +14,28 @@ public enum TarefaPrioridade {
             this.id = id;
     }
 
+   public static TarefaPrioridade integerToPrioridade(Integer id){
+        for(TarefaPrioridade tarefa : TarefaPrioridade.values()){
+            if(tarefa.getId() == id){
+                return tarefa;
+            }
+        }
+        return null;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
