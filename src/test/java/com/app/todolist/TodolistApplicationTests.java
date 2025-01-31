60510fe8ddfbe31cf3de8616e8d21edc3bb4c652
JordanTrindade <jordandimst@gmail.com>
2025-01-31 18:20:25 -0300

    Update TodolistApplicationTests.java
    Adicionando teste created

--- src/test/java/com/app/todolist/TodolistApplicationTests.java
+++ src/test/java/com/app/todolist/TodolistApplicationTests.java
@@ -1,24 +1,44 @@
 package com.app.todolist;
 
+import com.app.todolist.model.task.Tarefa;
+import com.app.todolist.model.task.TarefaPrioridade;
+import com.app.todolist.model.task.TarefaStatus;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodolistApplicationTests {
 
-	private final WebTestClient webTestClient;
+	@Autowired
+	private WebTestClient webTestClient;
+	private static final String TAREFAS_URI = "/tarefas";
 
-    TodolistApplicationTests(WebTestClient webTestClient) {
-        this.webTestClient = webTestClient;
-    }
+	@Test
+	void testCreatedSuccess() {
+		Tarefa tarefa = createTestTarefa();
 
-    @Test
-	void contextLoads() {
+		webTestClient.post()
+				.uri(TAREFAS_URI)
+				.bodyValue(tarefa)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$.length()").isEqualTo(1)
+				.jsonPath("$.[0].titulo").isEqualTo(tarefa.getTitulo())
+				.jsonPath("$.[0].descricao").isEqualTo(tarefa.getDescricao())
+				.jsonPath("$.[0].prioridade").isEqualTo(tarefa.getPrioridade().toString())
+				.jsonPath("$.[0].status").isEqualTo(tarefa.getStatus().toString());
 	}
 
-	void testCreatedSuccess   (){
-
+	private Tarefa createTestTarefa() {
+		return new Tarefa(
+				"tarefa 1",
+				"descricao",
+				TarefaPrioridade.integerToPrioridade(1),
+				TarefaStatus.integerToTarefaStatus(2)
+		);
 	}
-
 }
